2c694b1b8acc39e528aa1c62f9fffb885b516d7f
Jeremy Möglich <jeremy.moeglich@gmail.com>
2021-10-15 14:38:21 +0200

    added callback

--- src/routes/test_endpoint.ts
+++ src/routes/test_endpoint.ts
@@ -2,9 +2,13 @@
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 
+function delayed() {
+    console.log('1 Second passed')
+}
+
 export async function get({ body }) {
     console.log('request')
-
+    setTimeout(delayed, 1000)
 	return {
 		status: 200
 	};
